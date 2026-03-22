16b108fdd9b93450a5fbe3780e110df3e7f983e7
TraitPhoe <feniksfeniksovic@gmail.com>
2026-03-23 00:30:23 +0300

    Первая итерация проекта по OpenCV
    Функционал:
    1) Вращение изображения на заданное количество градусов
    2) Конвертация изображения в чёрно-белый цвет

---
+++ OpenCV_proj/main.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('images/test2.jpg')
+
+### вращение изображения
+def rotate(image, angle):
+    height, width = image.shape[:2]
+    rotation_point = (width//2, height//2)
+    mat = cv.getRotationMatrix2D(rotation_point, angle, 1)
+    return cv.warpAffine(image, mat, (width, height))
+
+### конвертация изображения в чёрнобелый
+def grayscale(image):
+    result = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+    return result
+
+### программа
+img = rotate(img, 90)
+img = grayscale(img)
+
+cv.imwrite("processed_images/Test.png", img)
+cv.imshow("Result", img)
+cv.waitKey(0)
