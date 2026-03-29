0a4bbb311e636f1bac1334cbcb3fbbd97f529d38
emotionalzombie <lokichit@mail.ru>
2026-03-29 20:00:29 +0800

    feat: add new getter, update json builder logic, and improve gitignore

--- ocrnlp/progress_ocr.py
+++ ocrnlp/progress_ocr.py
@@ -38,115 +38,88 @@ def get_pattern(doc_type, feild, pattern_index=0):
     pattern_key = patterns["doc_schemas"][doc_type][feild][pattern_index]
     return patterns["pattern_lib"][pattern_key]
 
-print(get_pattern("doc_type2", "reg_num"))
-
-def get_full_name(text):
+def get_full_name(text, pattern):
     """получение фио"""
     if not isinstance(text, str):
         return None
-    match = re.search(
-        r"что\s([А-ЯЁ][а-яё]+)\s([А-ЯЁ][а-яё]+)\s([А-ЯЁ][а-яё]+)",
-        text,
-        re.IGNORECASE
-        )
+    match = re.search(pattern, text, re.IGNORECASE)
     return " ".join(match.groups()) if match else None
 
-def get_reg_num(text):
+def get_reg_num(text, pattern):
     """получение регистрационного номера"""
     if not isinstance(text, str):
         return None
-    match = re.search(
-        r"Регистрационный\s+номер\s+(\d+-\d+)",
-        text,
-        re.IGNORECASE
-        )
+    match = re.search(pattern, text, re.IGNORECASE)
     return match.group(1) if match else None
 
-def get_own_date(text):
+def get_own_date(text, pattern):
     """получение даты получения"""
     if not isinstance(text, str):
         return None
-    match = re.search(
-        r"выдачи\s+(\d{2}\.\d{2}\.\d{4})",
-        text,
-        re.IGNORECASE
-        )
+    match = re.search(pattern, text, re.IGNORECASE)
     return match.group(1) if match else None
         
 
-def get_doc_num(text):
+def get_doc_num(text, pattern):
     """получение номера документа"""
     if not isinstance(text, str):
         return None
-    match = re.search(
-        r"О\s+ПОВЫШЕНИИ\s+КВАЛИФИКАЦИИ\s+(\d+)",
-        text,
-        re.IGNORECASE
-        )
+    match = re.search(pattern, text, re.IGNORECASE)
     return match.group(1) if match else None
 
-def get_date_from_to(text, flg):
+def get_date_from_to(text, pattern, flg):
     """получение даты от/до"""
     if flg not in (0,1):
         raise ValueError("1 или 0 надо")
     if not isinstance(text, str):
         return None
-    match = re.search(
-        r"с\s+(\d{2}\s+[а-яё]+\s+\d{4}\s+г\.)\sпо\s+(\d{2}\s+[а-яё]+\s+\d{4}\s+г\.)",
-        text,
-        re.IGNORECASE
-        )
+    match = re.search(pattern, text, re.IGNORECASE)
     if flg == 0:
         return match.group(1) if match else None 
     else: 
         return match.group(2) if match else None 
     
 
-def get_city_name(text):
+def get_city_name(text, pattern):
     """получение названия города"""
     if not isinstance(text, str):
         return None
-    match = re.search(
-        r"Город\s([А-ЯЁ][а-яё]+)",
-        text,
-        re.IGNORECASE
-        )
+    match = re.search(pattern, text, re.IGNORECASE)
     return match.group(1) if match else None
 
 
-def get_new_spec(text):
+def get_new_spec(text, pattern):
     """получение обученой специальности"""
     if not isinstance(text, str):
         return None
-    match = re.search(
-        r"программе\s+«([^»]+)»",
-        text,
-        re.IGNORECASE
-        )
+    match = re.search(pattern, text, re.IGNORECASE)
     return match.group(1) if match else None
 
-def get_hours_num(text):
+def get_hours_num(text,pattern):
     if not isinstance(text, str):
         return None
-    match = re.search(
-        r"объеме\s+(\d+)",
-        text,
-        re.IGNORECASE
-        )
+    match = re.search(pattern, text, re.IGNORECASE)
+    return match.group(1) if match else None
+
+def get_organization(text, pattern):
+    if not isinstance(text,str):
+        return None
+    match = re.search(pattern,text, re.IGNORECASE)
     return match.group(1) if match else None
     
 def build_json(text,save_file=False):
     text = normalize_text(text)
     jsonn = {
-        "full_name":get_full_name(text),
-        "own_date":get_own_date(text),
-        "reg_num": get_reg_num(text),
-        "doc_num": get_doc_num(text),
-        "date_from": get_date_from_to(text,0),
-        "date_to": get_date_from_to(text,1),
-        "city_name": get_city_name(text),
-        "new_spec": get_new_spec(text),
-        "hours_num": get_hours_num(text)
+        "full_name":get_full_name(text,get_pattern("doc_type1", "full_name")),
+        "own_date":get_own_date(text,get_pattern("doc_type1", "own_date")),
+        "reg_num": get_reg_num(text,get_pattern("doc_type1", "reg_num")),
+        "doc_num": get_doc_num(text,get_pattern("doc_type1", "doc_num")),
+        "date_from": get_date_from_to(text,get_pattern("doc_type1", "date_from_to"), 0),
+        "date_to": get_date_from_to(text,get_pattern("doc_type1", "date_from_to"), 1),
+        "city_name": get_city_name(text,get_pattern("doc_type1", "city_name")),
+        "new_spec": get_new_spec(text,get_pattern("doc_type1", "new_spec")),
+        "hours_num": get_hours_num(text,get_pattern("doc_type1", "hours_num")),
+        "organization" : get_organization(text, get_pattern("doc_type1", "organization"))
     }
     if save_file:
         with open("result.json", "w", encoding="utf-8") as f:
