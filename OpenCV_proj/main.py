c25446cabef8d8d14a6c7d25d91ceb3c9761829a
TraitPhoe <feniksfeniksovic@gmail.com>
2026-04-02 01:38:24 +0300

    Улучшено чтения изображения для OCR/NLP
    Чёрно-белый градиент заменён на бинарный, добавлен апскейл фотографии

--- OpenCV_proj/main.py
+++ OpenCV_proj/main.py
@@ -2,48 +2,62 @@ import cv2 as cv
 import numpy as np
 import json
 
-### чтение png
-img = cv.imread('images/example.jpg')
-h, w = img.shape[:2]
-
-### чтение разметки
-file = open('images/json_main.json', 'r')
-data = json.load(file)
-
-### создание списка словарей с данными о точках
-point_data = []
-for i in data[3]['kp-1']:
-    point_data.append(i)
-
-### создание списка с координатами всех точек в пикселях
-points = np.array([
-    [point_data[0]['x']/100*w, point_data[0]['y']/100*h],
-    [point_data[1]['x']/100*w, point_data[1]['y']/100*h],
-    [point_data[2]['x']/100*w, point_data[2]['y']/100*h],
-    [point_data[3]['x']/100*w, point_data[3]['y']/100*h]
-], dtype=np.float32)
-
+h, w = 0, 0
+points = []
 ### создание точек назначения для трансформации (примерно лист а4)
 destination_points = np.array([[0, 0], [297, 0], [0, 210], [297, 210]], dtype=np.float32)
 
-### трансформация изображения
-def image_transform(target):
-    ## получение матрицы перспективы из разметки и точек назначения
-    mat = cv.getPerspectiveTransform(points, destination_points*2)
-    ## вывод нужного изображения
-    return cv.warpPerspective(target, mat, (297*2, 210*2))
+### чтение png
+def img_read(path, k):
+    result = cv.imread(path)
+    global h,w
+    h = result.shape[0]*k
+    w = result.shape[1]*k
+    result = cv.resize(result, (w, h))
+    return result
 
-### конвертация изображения в чёрнобелый
+### чтение разметки
+def markdown_read(path):
+    file = open(path, 'r')
+    data = json.load(file)
+
+    ### создание списка словарей с данными о точках
+    point_data = []
+    for i in data[4]['kp-1']:
+        point_data.append(i)
+
+### создание списка с координатами всех точек в пикселях
+    global points
+    points = np.array([
+        [point_data[0]['x']/100*w, point_data[0]['y']/100*h],
+        [point_data[1]['x']/100*w, point_data[1]['y']/100*h],
+        [point_data[2]['x']/100*w, point_data[2]['y']/100*h],
+        [point_data[3]['x']/100*w, point_data[3]['y']/100*h]
+    ], dtype=np.float32)
+
+
+### трансформация изображения
+def image_transform(target, k):
+    ## получение матрицы перспективы из разметки и точек назначения
+    mat = cv.getPerspectiveTransform(points, destination_points*k)
+    ## вывод нужного изображения
+    return cv.warpPerspective(target, mat, (297*k, 210*k))
+
+### конвертация изображения в чёрно-белый
 def grayscale(image):
     result = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+    _, result = cv.threshold(result, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
     return result
 
 ### программа
-img = image_transform(img)
+img = img_read('images/synth_example.png', 14)
+markdown_read('images/json_main.json')
+
+img = image_transform(img, 14)
 img = grayscale(img)
 
 ### запись готового результата в файл и показ изображения
-cv.imwrite("processed_images/Test.png", img)
+cv.imwrite("processed_images/Test.tiff", img)
 cv.imshow("Result", img)
 
 cv.waitKey(0)
