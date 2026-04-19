7e16aa63d9d446777bc2771e0a2865ed9a8c6cb0
TraitPhoe <feniksfeniksovic@gmail.com>
2026-04-19 23:22:39 +0300

    Update main.py

--- OpenCV_proj/main.py
+++ OpenCV_proj/main.py
@@ -1,10 +1,10 @@
 import cv2 as cv
 import numpy as np
 import json
+import pdf2image
 from pathlib import Path
 
 
-points = []
 ### создание точек назначения для трансформации (примерно лист а4)
 destination_points = np.array([[0, 0], [297, 0], [0, 210], [297, 210]], dtype=np.float32)
 
@@ -19,26 +19,25 @@ def read(img_path, json_path, k):
         img_output = cv.resize(img_output, (w,h))
     else: img_output = 0
     ## чтение джсона
-    file = open(json_path, "r")
-    data = json.load(file)
+    with open(json_path, "r") as file:
+        data = json.load(file)
 
     point_data = []
     for i in data[4]['kp-1']:
         point_data.append(i)
 
-    global points
+
     points = np.array([
         [point_data[0]['x'] / 100 * w, point_data[0]['y'] / 100 * h],
         [point_data[1]['x'] / 100 * w, point_data[1]['y'] / 100 * h],
         [point_data[2]['x'] / 100 * w, point_data[2]['y'] / 100 * h],
         [point_data[3]['x'] / 100 * w, point_data[3]['y'] / 100 * h]
     ], dtype=np.float32)
-
-    return img_output
+    return img_output, points
 
 
 ### трансформация изображения
-def image_transform(target, k):
+def image_transform(target, k, points):
     ## получение матрицы перспективы из разметки и точек назначения
     mat = cv.getPerspectiveTransform(points, destination_points*k)
     ## вывод нужного изображения
@@ -63,21 +62,19 @@ def preprocess(img_path="0.png", k=4):
     output_arr = []
     # программа
     for i in range(num):
-
         ### создание пути вывода
         output_dir = parent_dir / f"processed_images/{i}.tiff"
         img_dir = parent_dir / "images" / f"{i}.png"
         ### обработка изображения
-        img = read(str(img_dir), str(json_dir), k)
-        img = image_transform(img, k)
+        img, points = read(str(img_dir), str(json_dir), k)
+        img = image_transform(img, k, points)
         img = grayscale(img)
 
         ### запись готового результата в файл и показ изображения
         cv.imwrite(output_dir, img)
         output_arr.append(str(output_dir))
         #cv.imshow("Result", img)
-    #cv.waitKey(0)
-    print(output_arr)
+        #cv.waitKey(0)
     return output_arr
 
 if __name__ == "__main__":
